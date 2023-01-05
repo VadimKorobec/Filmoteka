0c797780fdef35792f27276f283cb497e5782309
VadimKorobec <_s_e_t__@ukr.net>
2023-01-05 20:14:32 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './js/darkmode';
+import './js/themoviedb';
+import './js/fetchAPI';
+import './js/pagination';
+
+import './js/render-gallery';
+// import './js/libraryStorage';
+import './js/modalTeam';
+import './js/searchInput';
+
+import './js/modal';
+import './js/trailer';
+import './js/filter';
+
+import './js/spinner';
+import './js/btn-up';
